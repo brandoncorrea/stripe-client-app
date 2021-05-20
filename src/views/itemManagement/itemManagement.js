78b14c528bfe5bb61355534fd33a92ef7d63dc50
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2021-05-20 19:14:48 -0400

    Update to handle undefined response

--- src/views/itemManagement/itemManagement.js
+++ src/views/itemManagement/itemManagement.js
@@ -14,18 +14,26 @@ export default class ItemManagement extends React.Component {
   componentDidMount = () =>
     ProductRepository
       .getAll()
-      .then(i => this.setState({ products: i }));
+      .then(i => {
+        if (i && i.length)
+          this.setState({ products: i });
+      });
+      
 
   render = () =>
     <div className="ItemManagement">
       <header className="ItemManagement-header">
-        {this.state.products.map(i =>
-          <Link to={Routes.editItem + '?id=' + i.id}>
-            <ItemTile
-              key={i.id}
-              name={i.name}
-              description={i.description} />
-          </Link>)}
+        {
+          this.state.products.length > 0
+            ? this.state.products.map(i =>
+              <Link to={Routes.editItem + '?id=' + i.id}>
+                <ItemTile
+                  key={i.id}
+                  name={i.name}
+                  description={i.description} />
+              </Link>)
+            : <p>No items to display.</p>
+        }
         <Link to={Routes.createItem} className="btn">New Item</Link>
       </header>
     </div>;
