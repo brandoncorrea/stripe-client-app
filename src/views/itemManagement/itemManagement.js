3410b12aecf7a7ff82fd479d67cbc46191f57072
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2021-05-20 19:17:41 -0400

    Simplify logic

--- src/views/itemManagement/itemManagement.js
+++ src/views/itemManagement/itemManagement.js
@@ -14,17 +14,15 @@ export default class ItemManagement extends React.Component {
   componentDidMount = () =>
     ProductRepository
       .getAll()
-      .then(i => {
-        if (i && i.length)
-          this.setState({ products: i });
-      });
-      
+      .then(i => this.setState({ products: i }));
 
   render = () =>
     <div className="ItemManagement">
       <header className="ItemManagement-header">
         {
-          this.state.products.length > 0
+          // Are there any items?
+          this.state.products && this.state.products.length > 0
+            
             ? this.state.products.map(i =>
               <Link to={Routes.editItem + '?id=' + i.id}>
                 <ItemTile
@@ -32,6 +30,7 @@ export default class ItemManagement extends React.Component {
                   name={i.name}
                   description={i.description} />
               </Link>)
+            
             : <p>No items to display.</p>
         }
         <Link to={Routes.createItem} className="btn">New Item</Link>
