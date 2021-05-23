490d40d7f4aa95d2bc5002a535967b1efa9197bb
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2021-05-23 00:14:32 -0400

    Refactor PriceCard

--- src/components/PriceCard.js
+++ src/components/PriceCard.js
@@ -16,25 +16,47 @@ export default class PriceCard extends Component {
     // AppRouter.navigate(Routes.UpdatePrice + '?priceId=' + this.state.price.id);
   }
 
+  getCurrencySymbol = () =>
+    this.state.price.currency !== 'usd' 
+      ? this.state.price.currency
+      : '$';
+  
+  getFormattedPrice = () => 
+    this.getCurrencySymbol() +
+    (this.state.price.unit_amount / 100)
+      .toFixed(2)
+      .toString()
+      .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+  getUnitText = () =>
+    this.state.price.billing_scheme === 'per_unit'
+      ? 'Each'
+      : this.state.price.billing_scheme;
+
+  getHeaderText = () =>
+    this.getFormattedPrice() +
+    ' ' +
+    this.getUnitText();
+
+  getDescription = () =>
+    this.state.price.type === 'one_time'
+      ? 'One Time'
+    : this.state.price.type === 'recurring'
+      ? 'Recurring'
+    : this.state.price.type;
+
+  getCardColor = () =>
+    this.state.price.active 
+      ? 'green' 
+      : 'red';
+  
   render = () => 
     <Card fluid 
       onClick={this.navigateToEditPrice}
-      color={
-        this.state.price.active 
-          ? 'green' 
-          : 'red'
-      }>
+      color={this.getCardColor()}>
       <Card.Content>
-        <Card.Header content={
-          (this.state.price.unit_amount / 100) +
-          ' ' +
-          this.state.price.currency +
-          ' ' +
-          this.state.price.billing_scheme
-        } />
-        <Card.Description content={
-          this.state.price.type
-        } />
+        <Card.Header content={this.getHeaderText()} />
+        <Card.Description content={this.getDescription()} />
       </Card.Content>
     </Card> 
 }
