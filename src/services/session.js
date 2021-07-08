d667ca59468cac26b9d5f2555f9a0abbe7a5a41b
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2021-07-08 18:08:56 -0400

    Fix getUserPermission function

--- src/services/session.js
+++ src/services/session.js
@@ -5,13 +5,18 @@ import * as appSettings from '../appSettings.json';
 
 const tokenName = 'google-token';
 
+// Sets the user auth token
 export const setSessionCookie = token => {
   Cookies.remove(tokenName);
   Cookies.set(tokenName, token, { expires: 14 });
 };
 
-export const removeSessionCookie = () => Cookies.remove(tokenName);
+// Deletes the google token from the cookies
+export const removeSessionCookie = () => 
+  Cookies.remove(tokenName);
 
+// Returns the current google token from the cookies
+// Returns null if it is not found or expired
 export function getSessionCookie() {
   var token = Cookies.get(tokenName);
   if (token === null || token === undefined)
@@ -23,6 +28,8 @@ export function getSessionCookie() {
   removeSessionCookie();
   return null;
 }
+
+// Returns the decoded user token
 export function getUser() {
   var token = getSessionCookie();
   if (token === null || token === undefined)
@@ -31,13 +38,16 @@ export function getUser() {
   return jwt_decode(token);
 }
 
+// Returns the user's current permission set
 export function getUserPermission() {
-  var email = getUser().email
-  if (email === undefined)
+  var currentUser = getUser();
+  if (currentUser === null || currentUser.email === undefined)
     return 0;
-  var user = appSettings.Users[email];
+
+  var user = appSettings.Users[currentUser.email];
   if (user === undefined)
     return 0;
+  
   return user.PermissionID;
 }
 
